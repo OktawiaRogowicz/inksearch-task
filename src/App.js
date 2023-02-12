57a9bf4c03722e099955eb7f92b862bd85802996
CodeSandbox <support@codesandbox.io>
2023-02-12 12:35:18 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const API_URL = `https://api.tcgdex.net/v2/en/cards`;
+const NUMBER_OF_CARDS = 10;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getImgSrc(image) {
+  return `${image}/high.webp`;
+}
+
+export default function App() {
+  const [cards, setCards] = useState([]);
+  const [mainCardId, setMainCardId] = useState(0);
+  const [cardDetails, setCardDetails] = useState(null);
+
+  const getCard = (card) => {
+    setCards((arr) => [...arr, card]);
+  };
+
+  const getCards = (cards) => {
+    const cardsCounter = cards.length;
+    for (let i = 0; i < NUMBER_OF_CARDS; i++) {
+      const cardId = getRandomInt(cardsCounter);
+      getCard(cards[cardId]);
+    }
+  };
+
+  const getPreviousId = () => {
+    if (mainCardId <= 0) return NUMBER_OF_CARDS - 1;
+    return mainCardId - 1;
+  };
+
+  const getNextId = () => {
+    if (mainCardId >= NUMBER_OF_CARDS - 1) return 0;
+    return mainCardId + 1;
+  };
+
+  const showPrevious = () => {
+    setMainCardId(getPreviousId());
+  };
+
+  const showNext = () => {
+    setMainCardId(getNextId());
+  };
+
+  function showCardDetails() {
+    if (cards && cards.length > 0)
+      fetch(`${API_URL}/${cards[mainCardId].id}`)
+        .then((res) => res.json())
+        .then((cardDetails) => {
+          setCardDetails(cardDetails);
+        });
+  }
+
+  const shuffleCards = () => {
+    setCards([]);
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((cards) => {
+        getCards(cards);
+      });
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  useEffect(() => {
+    showCardDetails();
+  }, [cards, mainCardId]);
+
+  return (
+    <div className="App">
+      <div className="heading">
+        <h1 className="title">Pokemon cards</h1>
+        <button onClick={shuffleCards} className="subtitle">
+          click here to shuffle
+        </button>
+      </div>
+      <div className="cards-container">
+        <button
+          onClick={showPrevious}
+          className="arrow arrow-left"
+        >{`<`}</button>
+        <button onClick={showNext} className="arrow arrow-right">
+          {`>`}
+        </button>
+        {cards && cards.length > 0 && (
+          <div className="cards">
+            <button onClick={showPrevious} className="card card-margin">
+              <img
+                key={`pokemon-card-previous`}
+                src={getImgSrc(cards[getPreviousId()].image)}
+              />
+            </button>
+            <button className="card">
+              <div className="card-active">
+                <img
+                  key={`pokemon-card-main`}
+                  src={getImgSrc(cards[mainCardId].image)}
+                />
+              </div>
+              {cardDetails && (
+                <div className="card-details">
+                  <div className="card-details-heading">
+                    <p className="card-name">
+                      {cardDetails.name}
+                      {cardDetails.hp && `, ${cardDetails.hp}HP`}
+                    </p>
+                  </div>
+                  <p className="card-rarity">{cardDetails.rarity}</p>
+                </div>
+              )}
+            </button>
+            <button onClick={showNext} className="card card-margin">
+              <img
+                key={`pokemon-card-next}`}
+                src={getImgSrc(cards[getNextId()].image)}
+              />
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/README.md
@@ -0,0 +1,29 @@
+# Zadanie rekrutacyjne od INKsearch ⚡
+
+> Celem zadania jest sprawdzenie podstawowych znajomości JS i React
+
+> Bez obaw zadanie jest proste i nie powinno sprawić Ci problemów, zapewne robiłeś już to nie jeden raz.
+
+> W zadaniu należy używać functional components.
+> Nie musisz dodawać żadnych dependencies, szkoda czasu.
+
+> Przy pierwszej zapisanej zmianie projektu, automatycznie utworzy sie unikalny link tylko dla Ciebie. Pamiętaj zapistwać zmiany.
+
+## Co bierzmy pod uwagę?
+
+- czytelność kodu
+- oczywiście czas, ale bez stresu to nie są wyścigi, po prostu to jest proste zadanie i liczymy że wyrobisz się w niezbyt absurdalnym czasie :)
+- aspekt wizualny strony UI, chodzi o czytelność; chaos nie jest wskazany
+
+## Zadanie:
+
+1. Zrobić fetch do publicznego api i wylistować 5 kart z otrzymanego response (przykładowo od 5 do 10, w tym Api pierwsze karty są słabej jakości).
+2. Wylistowane karty powinny być klikalne, po kliknięciu karty powinien wykonać się kolejny api fetch po detaliczne dane karty. Z otrzymanynch danych poniżej kart już wyświetlanych, wyświetlić dane klikniętej karty (name, rarity, hp).
+
+- Link do Api Docs: https://tcgdex.dev/
+
+## Po ukończeniu zadania:
+
+- wyśli link z codesandbox na mail: slawomir@inksearch.co
+
+Powodzenia!
